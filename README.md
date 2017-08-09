b40fdec8c1ff77ba912cd8000ffd2be5853fb1fe
Scott Hillman <hillmanov+github@gmail.com>
2017-08-08 18:38:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Crappy version of MobX
+
+Not really - just a very, very simple implementation for automatic observable tracking.
+
+Not meant to be a good solution, just an illustration of how things like this work conceptually. 
---
+++ index.js
@@ -0,0 +1,75 @@
+const person = {
+  firstName: 'Jimmy',
+  lastName: 'Cozza',
+  age: 18
+};
+
+const crappyMobx = {
+  inObserver: false,
+
+  observable(obj) {
+    const handler = {
+      observers: {},
+      get(target, name) {
+        if (crappyMobx.inObserver) {
+          this.observers[name] = this.observers[name] || [];
+          this.observers[name].push(crappyMobx.observer);
+        }
+        return target[name];
+      },
+      set(target, name, value) {
+        if (!crappyMobx.inAction) {
+          throw Error('Must be called in action');
+        }
+        target[name] = value;
+
+        if (this.observers[name]) {
+          this.observers[name].forEach(fn => fn());
+        }
+      }
+    };
+    return new Proxy(obj, handler);
+  },
+
+  autorun(observer) {
+    crappyMobx.inObserver = true;
+    crappyMobx.observer = observer;
+    observer();
+    crappyMobx.inObserver = false;
+    crappyMobx.observer = null;
+  },
+
+  reaction(register, reactionFn) {
+    crappyMobx.inObserver = true;
+    crappyMobx.observer = reactionFn;
+    register();
+    crappyMobx.inObserver = false;
+    crappyMobx.observer = null;
+  },
+
+  action(fn) {
+    crappyMobx.inAction = true;
+    fn();
+    crappyMobx.inAction = false;
+  }
+};
+
+const observablePerson = crappyMobx.observable(person);
+
+// crappyMobx.autorun(() => {
+//   console.log(observablePerson.firstName, observablePerson.lastName);
+// });
+
+crappyMobx.reaction(
+  () => [observablePerson.firstName, observablePerson.lastName],
+  () => console.log('Someone changed the first name', observablePerson.age)
+);
+
+crappyMobx.action(() => {
+  observablePerson.firstName = 'rewrewrew';
+  observablePerson.lastName = 'r32432432432';
+});
+
+
+
+
